ab5c2091d2772822cec0e410c509615e393448eb
Satyam Raj <rajsatyammm@gmail.com>
2023-04-14 20:35:48 +0530

    Create find Mid.java

---
+++ find Mid.java
@@ -0,0 +1,14 @@
+/*
+Find mid using slow-fast Approach
+*/
+
+public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
