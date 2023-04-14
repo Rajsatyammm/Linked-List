c1f077be7fa034e534c13abbe1a956b081e52518
Satyam Raj <rajsatyammm@gmail.com>
2023-04-15 00:47:58 +0530

    Create Remove Cycle.java

---
+++ Remove Cycle.java
@@ -0,0 +1,28 @@
+public void removeCycle() {
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+
+        slow = head;
+        Node prev = null;
+        while (fast != slow) {
+            prev = fast;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        // remove loop
+        prev.next = null;
+
+    }
